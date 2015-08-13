e628afe964c909e6b65b6d51b056551188fcb4d5
gogu <gogu9909@gmail.com>
2015-08-13 18:52:36 +0800

    Git-gutter config.

--- lisp/init-git-gutter.el
+++ lisp/init-git-gutter.el
@@ -3,4 +3,14 @@
 (global-git-gutter-mode t)
 (git-gutter:linum-setup)
 
+(custom-set-variables
+ ;; live update
+ '(git-gutter:update-interval 2)
+ ;; set looks
+ '(git-gutter:modified-sign " ")
+ '(git-gutter:uncahnged-sig " "))
+
+(set-face-background 'git-gutter:modified "purple")
+(set-face-background 'git-gutter:unchanged "yellow")
+
 (provide 'init-git-gutter)
