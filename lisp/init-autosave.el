25bed3035c8e5c35a2314fc43b2ffe4853cec7ab
gogu <gogu9909@gmail.com>
2015-09-07 13:27:53 +0800

    Fixed autosave.

--- lisp/init-autosave.el
+++ lisp/init-autosave.el
@@ -1,6 +1,7 @@
+;;; https://snarfed.org/gnu_emacs_backup_files
 (custom-set-variables
- '(backup-directory-alist '(("." . "~/.emacs.d/backups/")))
- '(auto-save-file-name-transforms '(("." "~/.emacs.d/autosaves/\\1 t"))))
+ '(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
+ '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t))))
 
 (make-directory "~/.emacs.d/autosaves/" t)
 (provide 'init-autosave)
